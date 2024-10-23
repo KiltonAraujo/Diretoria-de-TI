ac3009080dd7928a69ba34a82818393482d0fd23
Kilton Jhonathan de Araújo <01652863427@ifrn.local>
2024-10-23 17:39:50 -0300

    home concluida com responsividade e iniciação da pagina sobre - 23.10.2024

--- src/javascript/dti.js
+++ src/javascript/dti.js
@@ -1,17 +1,53 @@
-function animateCounter(target, elementId, duration) {
-    const element = document.getElementById(elementId);
-    const start = 0;
-    const end = target;
-    let current = start;
+// Animação dos numeros da home - sec-numeros
 
-    const timer = setInterval(() => {
-        current++;
-        element.textContent = current;
-        if (current === end) {
-            clearInterval(timer);
+let contadoresAnimados = false; // Variável de controle
+
+function animarContador(valorFinal, idElemento, duracaoTotal) {
+    const elemento = document.getElementById(idElemento);
+
+    // Verifica se o elemento está visível na tela antes de começar a animação
+    if (!elementoVisivelNaTela(elemento) || contadoresAnimados) {
+        return; // Se o elemento não estiver visível ou já foi animado, não faz nada
+    }
+
+    let valorAtual = 0;
+    const tempoPasso = duracaoTotal / valorFinal;
+
+    const temporizador = setInterval(() => {
+        valorAtual++;
+        elemento.textContent = valorAtual;
+        if (valorAtual === valorFinal) {
+            clearInterval(temporizador);
+            contadoresAnimados = true;
         }
-    }, duration);
+    }, tempoPasso);
 }
 
-animateCounter(10, 'counter', 200);
-animateCounter(10, 'counter2', 200);
+function elementoVisivelNaTela(elemento) {
+    const posicao = elemento.getBoundingClientRect();
+    return posicao.top >= 0 && posicao.bottom <= window.innerHeight;
+}
+
+window.addEventListener('scroll', () => {
+    if (!contadoresAnimados) {
+        animarContador(2, 'setores', 5000);
+        animarContador(21, 'atendentes', 5000);
+        animarContador(49, 'servicos', 5000);
+        animarContador(13, 'chamados-milhares', 5000);
+        animarContador(981, 'chamados-centenas', 5000);
+        animarContador(97 , 'porcentagem-dezena', 5000);
+        animarContador(7 , 'porcentagem-unidade', 5000);
+    }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (!contadoresAnimados) {
+        animarContador(2, 'setores', 5000);
+        animarContador(21, 'atendentes', 5000);
+        animarContador(49, 'servicos', 5000);
+        animarContador(13, 'chamados-milhares', 5000);
+        animarContador(981, 'chamados-centenas', 5000);
+        animarContador(97 , 'porcentagem-dezena', 5000);
+        animarContador(7 , 'porcentagem-unidade', 5000);
+    }
+});
